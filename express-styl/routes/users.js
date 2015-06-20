d0ae2f6fa7043148ea7488d195348a3f06c82661
overkazaf <overkazaf@gmail.com>
2015-06-20 21:11:49 +0800

    test router

--- express-styl/routes/users.js
+++ express-styl/routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+	res.send('<h1>Welcome to User Page</h1>');
+ 	console.log('in user');
 });
 
 module.exports = router;
