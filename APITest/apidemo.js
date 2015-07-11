c08fb33a7ff334ced85beaa4ccc29faf3a870e82
overkazaf <overkazaf@gmail.com>
2015-07-11 19:04:11 +0800

    add test demo for node api

---
+++ APITest/apidemo.js
@@ -0,0 +1,15 @@
+var log = console.log;
+
+// __dirname
+log(__dirname);
+
+// __filename
+log(__filename);
+
+
+// time
+console.time('100-elements');
+for (var i = 0; i < 100000000; i++) {
+  ;
+}
+console.timeEnd('100-elements');
