3427a407209b2b08f5398330d95d4e53346a9ed3
overkazaf <overkazaf@gmail.com>
2015-07-16 00:09:38 +0800

    add os api test cases

--- APITest/ES6.js
+++ APITest/ES6.js
@@ -6,6 +6,20 @@ console.log(o);
 console.log([] == []);
 
 // crypto
+var crypto = require('crypto');
 var ciphers = crypto.getCiphers();
 
-console.log(ciphers);
+console.log(crypto.getHashes());
+
+// os
+
+var os = require('os');
+console.log(os.tmpdir());
+console.log(os.endianness());
+console.log(os.hostname());
+console.log(os.type());
+console.log(os.platform());
+console.log(os.release());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.cpus());
