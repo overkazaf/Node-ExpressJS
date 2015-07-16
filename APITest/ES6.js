f2f49eec145d8319534d8129749016ed66a6af99
overkazaf <overkazaf@gmail.com>
2015-07-16 22:30:23 +0800

    test swap fn

--- APITest/ES6.js
+++ APITest/ES6.js
@@ -13,13 +13,25 @@ console.log(crypto.getHashes());
 
 // os
 
-var os = require('os');
-console.log(os.tmpdir());
-console.log(os.endianness());
-console.log(os.hostname());
-console.log(os.type());
-console.log(os.platform());
-console.log(os.release());
-console.log(os.freemem());
-console.log(os.totalmem());
-console.log(os.cpus());
+// var os = require('os');
+// console.log(os.tmpdir());
+// console.log(os.endianness());
+// console.log(os.hostname());
+// console.log(os.type());
+// console.log(os.platform());
+// console.log(os.release());
+// console.log(os.freemem());
+// console.log(os.totalmem());
+// console.log(os.cpus());
+
+
+var swap = function (a, b){
+	return [b, a];
+}
+
+// pass an array to swap values
+var a = 1;
+var b = 2;
+var c  = [a, b];
+c = swap(a,b);
+console.log(c);
