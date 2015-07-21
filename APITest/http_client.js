f63983e1f29c6e45299fcd1765ac218ec8a8f9f8
overkazaf <overkazaf@gmail.com>
2015-07-21 23:17:49 +0800

    add http request and require test cases

---
+++ APITest/http_client.js
@@ -0,0 +1,21 @@
+var http = require('http');
+
+var options = {
+	hostname : '127.0.0.1',
+	port : 1234,
+	path : '/',
+	method : 'GET'
+};
+
+var req = http.request(options, function (res){
+	console.log('Status code:' + res.statusCode);
+	console.log('Headers:' + JSON.stringify(res.headers));
+
+	res.setEncoding('utf8');
+
+	res.on('data', function (chunk){
+		console.log(chunk);
+	});
+});
+
+req.end();
